47375c73a4c7968a90a59978183fcb579142c5d7
Ega Rifqi Saputra <egarifqi@gmail.com>
2018-04-04 01:46:00 +0700

    body 3D done

--- tesopengl.c
+++ tesopengl.c
@@ -1,34 +1,24 @@
 #include <GL/glut.h>
 #include <math.h>
+#include <windows.h>
 #include <unistd.h>
 
 
 float angle1 = 0.0f;
-void drawCircle1(GLfloat x, GLfloat y, GLfloat radius)
-{
-	int i;
-	int PI = 3.14159;
-	int triangleAmount = 10;
-	GLfloat twicePi = 2.0f * PI;
-	
-	//glTranslatef(0.0f,1.0f,0.0f);
-	glRotatef(angle1, 0.0f, 0.0f, 1.0f);
-	glEnable(GL_LINE_SMOOTH);
-	glLineWidth(1.0);
-	
-	glBegin(GL_LINES);
-	glColor4f(0.0, 0.0, 0.0, 1.0);
-	for(i = 0; i <= triangleAmount; i++)
-	{
-		glVertex2f( x, y);
-		glVertex2f(x + (radius * cos(i * twicePi / triangleAmount)), y + (radius * sin(i * twicePi / triangleAmount)));
-	}
-	glEnd();
-	angle1+=0.1f;
+
+char title[] = "3D Shapes";
+
+void initGL(){
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
 float angle2 = 0.0f;
-void drawCircle2(GLfloat x, GLfloat y, GLfloat radius)
+void drawCircle(GLfloat x, GLfloat y, GLfloat radius)
 {
 	int i;
 	int PI = 3.14159;
@@ -71,15 +61,16 @@ int x2 = 390;
 
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+	glMatrixMode(GL_MODELVIEW);
+	
+	glLoadIdentitty();
+	glTranslatef(1.5f, 0.0f, -7.0f);
 	//glLoadIdentity();
 	
 	drawBody();
 	//glRotatef(angle, 1.0f, 0.0f, 0.0f);
-	drawCircle2(250,300,30);
-	drawCircle2(390,300,30);
-	drawCircle1(250,300,30);
-	drawCircle1(390,300,30);
+	drawCircle(250,300,30);
+	drawCircle(390,300,30);
 	/*
 	
 	glColor3f(0,1,0.8);
@@ -113,11 +104,9 @@ int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1280,1280);
-	glutCreateWindow("Open GL Test");
+	glutCreateWindow(title);
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
-	gluOrtho2D(0,640,0,640);
-	glClearColor(0.5,0.7,0.5,0);
 	glutMainLoop();
 	return 0;
 }
