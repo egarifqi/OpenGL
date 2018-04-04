6e20600e9aac8ecf7a5f08263dc5694abbb530f4
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2018-04-04 07:17:13 +0700

    add keyboard and mouse function

--- Tugas2.cpp
+++ Tugas2.cpp
@@ -3,10 +3,17 @@
  */
 #include <windows.h>  // for MS Windows
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include <math.h>
  
 /* Global variables */
 char title[] = "3D Shapes";
- 
+float x=0.0f,y=0.0f, z=5.0f;
+float deltaMoveX = 0, deltaMoveY = 0, deltaMoveZ = 0;
+float lx=0.1f, ly=0.1f, lz=-5.0f;
+float angle = 0.0f;
+float deltaAngle=0.0f;
+int xOrigin = -1;
+
 /* Initialize OpenGL Graphics */
 void initGL() {
    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
@@ -17,9 +24,17 @@ void initGL() {
    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
 }
  
+void computePos(){
+	x += deltaMoveX * lx * 0.1f;
+    y += deltaMoveY * ly * 0.1f;
+	z += deltaMoveZ * lz * 0.1f;
+}
 /* Handler for window-repaint event. Called back when the window first appears and
    whenever the window needs to be re-painted. */
 void display() {
+
+    computePos();
+
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
  
@@ -27,7 +42,10 @@ void display() {
    glLoadIdentity();                 // Reset the model-view matrix
    glTranslatef(0.0f, -0.5f, -7.0f);  // Move right and into the screen
  
- 
+    gluLookAt(x,y,z,
+            x+lx,y+ly,z+lz,
+            0.0f,1.0f,0.0f);
+
    glBegin(GL_QUAD_STRIP);
    	  //FACE
    	  
@@ -121,7 +139,6 @@ void display() {
       glVertex3f(1.3f, -0.5f,  1.0f);
       glVertex3f(1.3f, -0.5f, -1.0f);
    glEnd();  // End of drawing color-cube
- 
    
  
    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
@@ -142,6 +159,60 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
    glLoadIdentity();             // Reset
    // Enable perspective projection with fovy, aspect, zNear and zFar
    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+   glMatrixMode(GL_MODELVIEW);
+}
+
+void pressKey(int key, int xx, int yy) {
+
+	switch (key) {
+		case GLUT_KEY_LEFT : deltaMoveX = -0.5f; break;
+		case GLUT_KEY_RIGHT : deltaMoveX = 0.5f; break;
+		case GLUT_KEY_UP : deltaMoveY = 0.5f; break;
+		case GLUT_KEY_DOWN : deltaMoveY = -0.5f; break;
+        case GLUT_KEY_PAGE_UP : deltaMoveZ = 0.2f; break;
+        case GLUT_KEY_PAGE_DOWN : deltaMoveZ = -0.2f; break;
+	}
+}
+
+void releaseKey(int key, int x, int y) {
+
+	switch (key) {
+		case GLUT_KEY_LEFT :
+		case GLUT_KEY_RIGHT : 
+		case GLUT_KEY_UP :
+		case GLUT_KEY_DOWN : 
+        case GLUT_KEY_PAGE_UP : 
+        case GLUT_KEY_PAGE_DOWN : deltaMoveX = 0;deltaMoveY = 0;deltaMoveZ = 0;break;
+	}
+}
+
+void mouseMove(int x, int y) { 	
+
+    // this will only be true when the left button is down
+    if (xOrigin >= 0) {
+
+		// update deltaAngle
+		deltaAngle = (x - xOrigin) * 0.001f;
+		// update camera's direction
+		lx = sin(angle + deltaAngle);
+		lz = -cos(angle + deltaAngle);
+	}
+}
+
+void mouseButton(int button, int state, int x, int y) {
+
+	// only start motion if the left button is pressed
+	if (button == GLUT_LEFT_BUTTON) {
+
+		// when the button is released
+		if (state == GLUT_UP) {
+			angle += deltaAngle;
+			xOrigin = -1;
+		}
+		else  {// state = GLUT_DOWN
+			xOrigin = x;
+		}
+	}
 }
  
 /* Main function: GLUT runs as a console application starting at main() */
@@ -151,8 +222,16 @@ int main(int argc, char** argv) {
    glutInitWindowSize(640, 480);   // Set the window's initial width & height
    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
    glutCreateWindow(title);          // Create window with the given title
+   
    glutDisplayFunc(display);       // Register callback handler for window re-paint event
    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
+   glutIdleFunc(display);
+   glutSpecialFunc(pressKey);
+   glutIgnoreKeyRepeat(1);
+   glutSpecialUpFunc(releaseKey);
+   glutMouseFunc(mouseButton);
+   glutMotionFunc(mouseMove);
+
    initGL();                       // Our own OpenGL initialization
    glutMainLoop();                 // Enter the infinite event-processing loop
    return 0;
