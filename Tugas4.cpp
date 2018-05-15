55f5104625d72de931114f7331c7be0478434eed
alcreia <scizor.scythe@gmail.com>
2018-05-15 22:26:09 +0700

    rain added

--- Tugas4.cpp
+++ Tugas4.cpp
@@ -7,6 +7,7 @@
 #include "RgbImage.h"
 #include "RgbImage.cpp"
  
+#define MAX_PARTICLES 1000
 /* Global variables */
 char title[] = "3D Shapes";
 float x=0.0f,y=0.0f, z=5.0f;
@@ -17,7 +18,49 @@ float deltaAngle=0.0f;
 int xOrigin = -1;
 char filename[] = "./salt_on_spoon.bmp";
 GLuint   texture[1];
+float slowdown = 2.0;
+float velocity = 0.0;
+int loop;
+int fall;
 
+typedef struct {
+  // Life
+  bool alive;	// is the particle alive?
+  float life;	// particle lifespan
+  float fade; // decay
+  // color
+  float red;
+  float green;
+  float blue;
+  // Position/direction
+  float xpos;
+  float ypos;
+  float zpos;
+  // Velocity/Direction, only goes down in y dir
+  float vel;
+  // Gravity
+  float gravity;
+}particles;
+
+particles par_sys[MAX_PARTICLES];
+
+void initParticles(int i) {
+    par_sys[i].alive = true;
+    par_sys[i].life = 1.0;
+    par_sys[i].fade = float(rand()%100)/1000.0f+0.003f;
+
+    par_sys[i].xpos = (float) (rand() % 21) - 10;
+    par_sys[i].ypos = 10.0;
+    par_sys[i].zpos = (float) (rand() % 21) - 10;
+
+    par_sys[i].red = 0.5;
+    par_sys[i].green = 0.5;
+    par_sys[i].blue = 1.0;
+
+    par_sys[i].vel = velocity;
+    par_sys[i].gravity = -0.8;//-0.8;
+
+}
 //LIGHTING FUNCTION
 void AmbientLighting();
 void PointLight(const float x, const float y, const float z, const float amb, const float diff, const float spec);
@@ -86,6 +129,46 @@ void initGL() {
    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);  // Nice perspective corrections
    
    glEnable(GL_NORMALIZE);
+   
+   // Initialize particles
+    for (loop = 0; loop < MAX_PARTICLES; loop++) {
+        initParticles(loop);
+    }
+}
+
+// For Rain
+void drawRain() {
+  float x, y, z;
+  for (loop = 0; loop < MAX_PARTICLES; loop=loop+2) {
+    if (par_sys[loop].alive == true) {
+      x = par_sys[loop].xpos;
+      y = par_sys[loop].ypos;
+      z = par_sys[loop].zpos - 2.0;
+
+      // Draw particles
+      glColor3f(0.5, 0.5, 1.0);
+      glBegin(GL_LINES);
+        glVertex3f(x, y, z);
+        glVertex3f(x, y+0.5, z);
+      glEnd();
+
+      // Update values
+      //Move
+      // Adjust slowdown for speed!
+      par_sys[loop].ypos += par_sys[loop].vel / (slowdown*1000);
+      par_sys[loop].vel += par_sys[loop].gravity;
+      // Decay
+      par_sys[loop].life -= par_sys[loop].fade;
+
+      if (par_sys[loop].ypos <= -10) {
+        par_sys[loop].life = -1.0;
+      }
+      //Revive
+      if (par_sys[loop].life < 0.0) {
+        initParticles(loop);
+      }
+    }
+  }
 }
 
 void loadTextureFromFile(char *filename)
@@ -459,6 +542,8 @@ void display() {
  
     //glFlush();
    glDisable(GL_TEXTURE_2D);
+   
+   drawRain();
 
    glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
